101a9bc6eceb8ff47f2dc3471f0671c403c3dd19
Hans Jakob Håland <hansjakob.haland99@gmail.com>
2024-02-19 16:14:10 +0100

    Adds hand classifier

---
+++ card_deck.py
@@ -0,0 +1,51 @@
+import random
+
+class Card:
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        
+    def __str__(self):
+        rank_symbols = {"10":"T", "11":"J", "12":"Q", "13":"K", "14":"A"}
+        rank = str(self.rank)
+        if rank in rank_symbols:
+            rank = rank_symbols[rank]
+        return rank + self.suit
+    
+    def get_suit(self):
+        return self.suit
+    
+    def get_rank(self):
+        return self.rank
+    
+    
+class CardDeck:
+        def __init__(self):
+            self.cards = []
+            for suit in ['S', 'H', 'D', 'C']:
+                for rank in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
+                    self.cards.append(Card(suit, rank))
+                    
+        def __str__(self):
+            return ' '.join(str(card) for card in self.cards)
+        
+        def shuffle(self):
+            random.shuffle(self.cards)
+            
+        def deal(self, n):
+            return [self.cards.pop() for _ in range(n)]
+        
+        def exclude(self, card):
+            pass
+        
+        
+        
+if __name__ == "__main__":
+    card_deck = CardDeck()
+    print(card_deck)
+    print(len(card_deck.cards))
+    card_deck.shuffle()
+    [print(card) for card in card_deck.deal(4)]
+    print(len(card_deck.cards))
+    print(card_deck)
---
+++ poker_oracle.py
@@ -0,0 +1,137 @@
+from card_deck import Card, CardDeck
+from itertools import combinations
+
+
+class PokerOracle:
+    
+    def __init__(self):
+        self.hand_rankings = {"royal_flush": 1,
+                              "straight_flush": 2,
+                              "four_of_a_kind": 3,
+                              "full_house": 4,
+                              "flush": 5,
+                              "straight": 6,
+                              "three_of_a_kind": 7,
+                              "two_pair": 8,
+                              "pair": 9,
+                              "high_card": 10}
+    
+    # NOTE: Easiest to evaluate set of 5 cards.
+    # Can define code for this and do recursive calls
+    # for cases with 6 or 7 cards, 5 cards at a time
+    def hand_classifier(self, card_set: list[Card]) -> tuple[str, int]: 
+        num_cards = len(card_set)
+        classification = ""
+        ranking = 0
+        if not num_cards == 5:
+            subsets = self.get_all_five_card_subsets(card_set)
+            for subset in subsets:
+                subset_classification, subset_ranking = self.hand_classifier(subset)
+                if subset_ranking < ranking:
+                    classification = subset_classification
+                    ranking = subset_ranking
+            return classification, ranking
+        else:
+            if self.is_flush(card_set):
+                if self.is_straight(card_set):
+                    if self.get_highest_card(card_set).get_rank() == 14: # TODO: Make this a constant? Or use something from the card_deck class?
+                        hand = "royal_flush"
+                        return hand, self.hand_rankings[hand] 
+                    hand = "straight_flush"
+                    return hand, self.hand_rankings[hand]
+                hand = "flush"
+                return hand, self.hand_rankings[hand]
+            if self.is_straight(card_set):
+                hand = "straight"
+                return hand, self.hand_rankings[hand]
+            if self.is_four_of_a_kind(card_set):
+                hand = "four_of_a_kind"
+                return hand, self.hand_rankings[hand]
+            if self.is_full_house(card_set):
+                hand = "full_house"
+                return hand, self.hand_rankings[hand]
+            if self.is_three_of_a_kind(card_set):
+                hand = "three_of_a_kind"
+                return hand, self.hand_rankings[hand]
+            paired_ranks = self.get_paired_ranks(card_set)
+            num_pairs = len(paired_ranks)
+            if num_pairs == 2:
+                hand = "two_pair"
+                return hand, self.hand_rankings[hand]
+            if num_pairs == 1:
+                hand = "pair"
+                return hand, self.hand_rankings[hand]
+            # Hand is high card if not any of the above
+            hand = "high_card"
+            return hand, self.hand_rankings[hand]
+                 
+    def get_all_five_card_subsets(self, card_set: list[Card]) -> tuple[list[Card]]: 
+        # Returns all possible five card subsets of a given
+        # 6 or 7 card set.
+        return list(combinations(card_set, 5))
+            
+    def is_flush(self, card_set: list[Card]) -> bool:
+        # Assumes 5 cards in card set
+        suits = [card.get_suit() for card in card_set]
+        num_different_suits = len(set(suits))
+        return num_different_suits == 1
+            
+    def is_straight(self, card_set: list[Card]) -> bool:
+        rank_set = [card.get_rank() for card in card_set]
+        rank_set.sort()
+        lower_set = rank_set[:-1]
+        upper_set = rank_set[1:]
+        for lower, upper in zip(lower_set, upper_set):
+            if not upper == lower + 1:
+                return False
+        return True
+        
+    def get_highest_card(self, card_set: list[Card]) -> Card:
+        return sorted(card_set, key=lambda card: card.get_rank())[-1]
+    
+    def count_ranks(self, card_set: list[Card]) -> list[int]:
+        # Possible card rank ranges from 2 - 14.
+        # Count number of ocurrences of each rank.
+        # Similar to procedure in Counting Sort
+        rank_count = [0] * 14
+        for card in card_set:
+            card_rank = card.get_rank()
+            rank_index = card_rank - 1
+            rank_count[rank_index] += 1
+        return rank_count
+    
+    def get_paired_ranks(self, card_set: list[Card]) -> int:
+            num_each_rank = self.count_ranks(card_set)
+            paired_ranks = []
+            for i in range(len(num_each_rank)):
+                if num_each_rank[i] == 2:
+                    paired_rank = i+1
+                    paired_ranks.append(paired_rank)
+            return paired_ranks
+        
+    def is_full_house(self, card_set: list[Card]):
+        num_each_rank = self.count_ranks(card_set)
+        return 2 in num_each_rank and 3 in num_each_rank
+    
+    def is_four_of_a_kind(self, card_set: list[Card]):
+        num_each_rank = self.count_ranks(card_set)
+        return 4 in num_each_rank
+            
+    def is_three_of_a_kind(self, card_set: list[Card]):
+        # Assumes full house is checked before this
+        num_each_rank = self.count_ranks(card_set)
+        return 3 in num_each_rank
+        
+
+if __name__ == "__main__":
+    # TODO: TESTING! Generate 2 cases of each (ish) poker hand, compare different
+    # combinaitions of hands and decide who wins.
+    poker_oracle = PokerOracle()
+    card_deck = CardDeck()
+    card_deck.shuffle()
+    card_set = card_deck.deal(7)
+    subsets = poker_oracle.get_all_five_card_subsets(card_set)
+    for subset in subsets:
+        print(type(subset))
+        for card in subset:
+            print(card)
